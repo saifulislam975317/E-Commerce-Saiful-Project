195962fd24186d0e22501600c334c0592b22f755
saiful islam <saifulislam975317@gmail.com>
2023-07-22 23:20:58 +0600

    e commerce project

---
+++ server/src/app.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+var bodyParser = require("body-parser");
+const createError = require("http-errors");
+const xssClean = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 4,
+  message: "To many request. Please try again later",
+});
+
+const isLoggedIn = (req, res, next) => {
+  const login = true;
+  if (login) {
+    next();
+  } else {
+    return res.status(401).json({ message: "please login first" });
+  }
+};
+// middleware
+app.use(isLoggedIn);
+app.use(limiter);
+app.use(xssClean());
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.status(200).send({
+    message: "Welcome to E-commerce saiful project",
+  });
+});
+
+app.get("/products", (req, res) => {
+  res.status(200).send({
+    message: "Products are returned",
+  });
+});
+
+app.get("/products/api", (req, res) => {
+  res.status(200).send({
+    message: "user profile api",
+  });
+});
+
+// client error handling
+app.use((req, res, next) => {
+  next(createError(404, "Error! route not found"));
+});
+
+// server error handling
+app.use((err, req, res, next) => {
+  return res.status(err.status || 500).json({
+    success: false,
+    message: err.message,
+  });
+});
+
+module.exports = app;
---
+++ server/src/server.js
@@ -0,0 +1,5 @@
+const app = require("./app");
+
+app.listen(5000, () => {
+  console.log(`server is running at http://localhost:5000`);
+});
